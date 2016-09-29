383ca7acfef26d4a303e50db4fc24910fae69b7c
Tom Scott <tom.scott@pwc.com>
2016-09-29 12:20:01 +1000

    update app.yaml and main.py with GAE project ID and bot token

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from google.appengine.api import urlfetch
 from google.appengine.ext import ndb
 import webapp2
 
-TOKEN = 'YOUR_BOT_TOKEN_HERE'
+TOKEN = '280588112:AAGBZro5Q7F-tlDdceJmonFNv7-MfGfb_hA'
 
 BASE_URL = 'https://api.telegram.org/bot' + TOKEN + '/'
 
